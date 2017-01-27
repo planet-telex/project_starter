3f497c7fb0f2f40b3be85d1f5a8848655029b920
Chris Hammock <crsrhmk@gmail.com>
2017-01-27 13:14:26 +0700

    real init

--- make.js
+++ make.js
@@ -1,89 +1,171 @@
 var exec = require('child_process').exec;
 var fs = require('fs');
-//var folder = process.argv[2];
+var folderName = process.argv[2];
 
-//exec('npm install jquery gsap backbone --save', function(err, stdout, stderr) {
+var config = {
+  dest: '~/Sites/'+folderName,
 
-//});
-
-exec('mkdir css js assets');
-exec('mkdir assets/img assets/fonts js/build js/build/router js/build/router/pages css/build css/build/pages');
-exec('touch .env .gitignore package.json gulpfile.js index.js index.html css/style.css css/build/global.css css/build/pages/samplepage.css js/bundle.js js/router/pages/index.js');
-
-var gulpfile = 'gulpfile.js';
-
-gulpFileText = `
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var minifyCss = require('gulp-clean-css');
-var plumber = require('gulp-plumber');
-
-function swallowError (error) {
-  console.log(error.toString())
-  this.emit('end')
-}
-
-gulp.task('css', function() {
-  return gulp.src(['./css/build/**/*.css'])
-  .pipe(plumber())
-  .pipe(concat('style.css'))
-  .pipe(gulp.dest('css'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch(['./css/build/**/*.css'], ['css']);
-});
-`;
-
-
-fs.writeFile(gulpfile, gulpFileText, function(err) {
-  if(err) {
-  	console.log('error writing gulpfile');
-  }
-});
-
-var pjText = `
-{
-  "name": "avenir",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "watch": "watchify index.js -o js/bundle.js -v"
-  },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "backbone": "^1.3.3",
-    "gsap": "^1.19.1",
-    "jquery": "^3.1.1"
-  },
-  "devDependencies": {
-    "browserify": "^13.3.0",
-    "gulp": "^3.9.1",
-    "gulp-clean-css": "^2.3.2",
-    "gulp-concat": "^2.6.1",
-    "gulp-plumber": "^1.1.0",
-    "watchify": "^3.8.0"
-  }
-}
-`;
-
-fs.writeFile('package.json', pjText, function(err){
-  if(err)
-    console.error(err);
-});
+};
 
 
 
-var gitignoreTxt = `*.DS_Store
-package.json
-gulpfile.js
-node_modules/
-public/assets/
-public/js/vendor/`;
 
-fs.writeFile('.gitignore', gitignoreTxt, function(err){
-  if(err)
-    console.error(err);
+fs.exists('./'+folderName, function(exists){
+    if(exists){
+      console.log('this directory already exists...exiting');
+
+    } else {
+
+
+      function addFolderNameToPaths(path) {
+        return path.replace(/ /g, ' '+folderName+'/');
+      }
+
+      //exec('npm install jquery gsap backbone --save', function(err, stdout, stderr) {
+
+      //});
+      var makeDirsTop = 'mkdir css js assets';
+      var makeDirsInner = 'mkdir assets/img assets/fonts js/build js/build/router js/build/router/pages css/build css/build/pages';
+      var makeFiles = 'touch .env .gitignore package.json gulpfile.js index.js index.html css/style.css css/build/global.css css/build/pages/samplepage.css js/bundle.js js/router/pages/index.js';
+
+      exec('mkdir '+folderName);
+      exec(addFolderNameToPaths(makeDirsTop));
+      exec(addFolderNameToPaths(makeDirsInner));
+      exec(addFolderNameToPaths(makeFiles));
+
+
+      //exec('mkdir assets/img assets/fonts js/build js/build/router js/build/router/pages css/build css/build/pages');
+      //exec('touch .env .gitignore package.json gulpfile.js index.js index.html css/style.css css/build/global.css css/build/pages/samplepage.css js/bundle.js js/router/pages/index.js');
+
+      var gulpfile = folderName+'/gulpfile.js';
+
+      gulpFileText = `
+      var gulp = require('gulp');
+      var concat = require('gulp-concat');
+      var minifyCss = require('gulp-clean-css');
+      var plumber = require('gulp-plumber');
+
+      function swallowError (error) {
+        console.log(error.toString())
+        this.emit('end')
+      }
+
+      gulp.task('css', function() {
+        return gulp.src(['./css/build/**/*.css'])
+        .pipe(plumber())
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('css'));
+      });
+
+      gulp.task('watch', function() {
+        gulp.watch(['./css/build/**/*.css'], ['css']);
+      });
+      `;
+
+
+      fs.writeFile(gulpfile, gulpFileText, function(err) {
+        if(err) {
+          console.log('error writing gulpfile');
+        }
+      });
+
+      var pjText = `{
+        "name": "${folderName}",
+        "version": "1.0.0",
+        "description": "",
+        "main": "index.js",
+        "scripts": {
+          "watch": "watchify index.js -o js/bundle.js -v"
+        },
+        "author": "planet telex vietnam",
+        "license": "ISC"
+      }
+      `;
+
+      var pJson = folderName+'/package.json';
+
+      fs.writeFile(pJson, pjText, function(err){
+        if(err)
+          console.error(err);
+      });
+
+      var gIgnore = folderName+'/.gitignore';
+
+
+      var gitignoreTxt = `*.DS_Store
+      package.json
+      gulpfile.js
+      node_modules/
+      public/assets/
+      public/js/vendor/`;
+
+      fs.writeFile(gIgnore, gitignoreTxt, function(err){
+        if(err)
+          console.error(err);
+      });
+
+
+
+
+      var landerPage = folderName+'/index.html';
+      var landerTemplate = `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Project</title>
+        <link rel="stylesheet" href="css/style.css">
+      </head>
+      <body>
+        <div id="wrapper">
+          <div id="first-wrapper" class="page-wrapper">
+            <div id="first-container" class="page-container">
+
+            </div><!--page container-->
+          </div><!--page wrapper-->
+        </div><!--wrapper-->
+        <script src="js/bundle.js"></script>
+      </body>
+      </html>`;
+
+
+      fs.writeFile(landerPage, landerTemplate, function(err){
+        if(err)
+          console.error(err);
+      });
+
+
+      var mdFile = folderName+'/notes.md';
+      var mdText = `#page generation
+      1.) node gen pageTitle
+      2.) add to router.js
+
+
+      in two terminal tabs:
+      gulp watch
+      npm run watch
+
+
+      #setup
+      each page has an outer wrapper and an inner container
+      wrapper displays as none and block
+      container animates opacity; can have an overlay over the bg image set in wrapper
+      `;
+
+      fs.writeFile(mdFile, mdText, function(err){
+        if(err)
+          console.error(err);
+      });
+
+
+      exec('cp gen.js '+folderName+'/gen.js');
+
+      //exec('npm --prefix ./'+folderName+'/ install jquery --save');
+      //exec('npm --prefix ./'+folderName+'/ install watchify --save-dev');
+
+      console.log('project setup; installing dependencies...');
+
+     
+    }
 });
